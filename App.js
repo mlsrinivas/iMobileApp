4f210199ec18f33024b419f897895b616370ca23
ICICI Bank Ltd <icicibankltd@POCKETS-ICICIs-MacBook-Pro.local>
2020-05-20 15:13:47 +0530

    src folder

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
-import Test from './Src/Screens/Test'
-import Header from './Src/Components/Header'
+import Test from './src/Screens/Test'
+import Header from './src/Components/Header'
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -12,13 +12,13 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        
-        <Stack.Screen 
-          name="Header" 
-          component={Header} 
+
+        <Stack.Screen
+          name="Header"
+          component={Header}
           options={{ title: 'HeadScreen' }}
-          />
-          <Stack.Screen
+        />
+        <Stack.Screen
           name="Test"
           component={Test}
           options={{ title: 'TestScreen' }}
